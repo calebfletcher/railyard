c5f33b3b0186938b957ac7764f37bb19f56598db
Caleb Fletcher <caleb@fletcher.cf>
2026-03-24 20:24:32 +1100

    timemaster: read id from si5340

--- boards/timemaster/firmware/src/main.rs
+++ boards/timemaster/firmware/src/main.rs
@@ -12,6 +12,12 @@ use rtic_monotonics::fugit::ExtU32;
 
 rtic_monotonics::systick_monotonic!(Mono, 10000);
 
+embassy_stm32::bind_interrupts!(struct Irqs {
+    I2C2 => embassy_stm32::i2c::EventInterruptHandler<embassy_stm32::peripherals::I2C2>, embassy_stm32::i2c::ErrorInterruptHandler<embassy_stm32::peripherals::I2C2>;
+    DMA1_CHANNEL1 => embassy_stm32::dma::InterruptHandler<embassy_stm32::peripherals::DMA1_CH1>;
+    DMA1_CHANNEL2_3  => embassy_stm32::dma::InterruptHandler<embassy_stm32::peripherals::DMA1_CH2>;
+});
+
 #[rtic::app(device = embassy_stm32::pac, peripherals = false, dispatchers = [SPI1, SPI2])]
 mod app {
 
@@ -21,7 +27,13 @@ mod app {
     struct Shared {}
 
     #[local]
-    struct Local {}
+    struct Local {
+        clk_i2c: embassy_stm32::i2c::I2c<
+            'static,
+            embassy_stm32::mode::Async,
+            embassy_stm32::i2c::mode::Master,
+        >,
+    }
 
     #[init]
     fn init(cx: init::Context) -> (Shared, Local) {
@@ -39,7 +51,19 @@ mod app {
         let health_led = embassy_stm32::gpio::Output::new(p.PB5, Level::Low, Speed::Low);
         blink_led::spawn(health_led).ok();
 
-        (Shared {}, Local {})
+        let clk_i2c = embassy_stm32::i2c::I2c::new(
+            p.I2C2,
+            p.PA11,
+            p.PA12,
+            p.DMA1_CH1,
+            p.DMA1_CH2,
+            Irqs,
+            embassy_stm32::i2c::Config::default(),
+        );
+
+        check_i2c::spawn().ok();
+
+        (Shared {}, Local { clk_i2c })
     }
 
     /// Blink one of the LEDs to show the system is alive
@@ -51,6 +75,21 @@ mod app {
             led.set_low();
             Mono::delay(900.millis()).await;
         }
+    }
+
+    #[task(local = [clk_i2c])]
+    async fn check_i2c(cx: check_i2c::Context<'_>) {
+        let i2c = cx.local.clk_i2c;
+
+        let address = 0b1110101;
+
+        i2c.write(address, &[0x0002]).await.unwrap();
+
+        let mut bytes = [0; 2];
+        i2c.read(address, &mut bytes).await.unwrap();
+        let chars = [bytes[1] >> 4, bytes[1] & 0xF, bytes[0] >> 4, bytes[0] & 0xF];
+
+        info!("read id {=[?]}", chars)
     }
 }
 
